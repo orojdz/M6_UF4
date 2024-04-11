853257eecd5f9f1650ad94d36cad84d194eef5ed
cf20orianarojas <rojasoriana260217@gmail.com>
2024-04-12 01:44:07 +0200

    App

---
+++ llista-tasques/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { LlistatTasques } from './components/LlistaTasques/LlistatTasques'
+import { Tasca } from './components/Tasca/Tasca'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <LlistatTasques/>
+    </>
+  )
+}
+
+export default App
