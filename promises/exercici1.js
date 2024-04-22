a76d37cd26cbdc61f07fb16bc9e03826f2b953ea
cf20orianarojas <rojasoriana260217@gmail.com>
2024-04-22 19:30:37 +0200

    funció promise

---
+++ promises/exercici1.js
@@ -0,0 +1,15 @@
+/* Crea una funció que donat un nombre per paràmetre et retorni, mitjançant una promesa si és
+divisible entre dos o no.*/
+
+function divisible(n) {
+    return new Promise((resolve, reject) => {
+        if (n % 2 === 0)
+            resolve(`${n} es divisible entre 2`)
+        else
+            reject(`${n} no es divisible entre 2`)
+    });
+}
+
+divisible(9)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
