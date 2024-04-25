828138434c250856c72bf2f73fd5365bfb9561d6
cf20orianarojas <rojasoriana260217@gmail.com>
2024-04-25 18:54:16 +0200

    WIP

---
+++ TMDB_Project/script.js
@@ -0,0 +1,81 @@
+// Claus
+const keys = {
+    api_key: '',
+    session_id: '',
+    account_id: ''
+}
+
+let moviesResult = document.getElementById("moviesResult");
+
+function setFav(id, favBool){
+    moviesResult.innerHTML="";
+
+    showFavs();
+}
+
+function showFavs(){
+    moviesResult.innerHTML="";
+
+}
+
+function searchMovies(query){
+    clearInput();
+    removeActive();
+}
+
+
+/* FUNCIONS D'INTERACCIÓ AMB EL DOM */
+
+// Click Favorites
+document.getElementById("showFavs").addEventListener("click", function(){
+    removeActive();
+    this.classList.add("active");
+
+    showFavs();
+})
+
+// Click Watchlist
+document.getElementById("showWatch").addEventListener("click", function(){
+    removeActive();
+    this.classList.add("active");
+
+    //showWatch();
+})
+
+/* Funcions per detectar la cerca d'una pel·lícula */
+// Intro a l'input
+document.getElementById("search").addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        searchMovies(this.value);
+    }
+});
+
+// Click a la lupa
+document.querySelector(".searchBar i").addEventListener("click", ()=>searchMovies(document.getElementById("search").value));
+
+// Netejar l'input
+document.getElementById("search").addEventListener('click', ()=>clearInput()); 
+
+function clearInput(){
+    document.getElementById("search").value="";
+}
+
+// Elimina l'atribut active del menú
+function removeActive(){
+    document.querySelectorAll(".menu li a").forEach(el=>el.classList.remove("active"));
+}
+
+/* Funció per printar les pel·lícules */
+function printMovie(movie, fav, watch){
+
+    let favIcon = fav ? 'iconActive' : 'iconNoActive';
+    let watchIcon = watch ? 'iconActive' : 'iconNoActive';
+
+    moviesResult.innerHTML += `<div class="movie">
+                                    <img src="https://image.tmdb.org/t/p/original${movie.poster_path}">
+                                    <h3>${movie.original_title}</h3>
+                                    <div class="buttons">
+                                        <a id="fav" onClick="setFav(${movie.id}, ${!fav})"><i class="fa-solid fa-heart ${favIcon}"></i></a>
+                                        <a id="watch" onClick="setWatch(${movie.id}, ${!watch})"><i class="fa-solid fa-eye ${watchIcon}"></i></a>
+                                    </div>`;
+}
