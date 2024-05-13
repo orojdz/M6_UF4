fae35114984750397f2009e0f6cf781ef7d49f28
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-13 19:08:19 +0200

    funciona movie card

--- movies_db/src/App.jsx
+++ movies_db/src/App.jsx
@@ -2,13 +2,22 @@ import './App.css'
 import { MovieCard }from './components/MovieCard';
 import { Welcome } from './components/Welcome'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import imagen from './img/cank.jpg'
 // import { Link } from 'react-router-dom'
 
 function App() {
+
   return (
       <Routes>
         <Route path="/" element={<Welcome username={"Oriana"} />} />
-        <Route path="/movies" element={<MovieCard title={"Cank"} image={"./img/cank.jpg"}/>} />
+        <Route path="/movies" element={
+          <MovieCard 
+            title={"Cank"} 
+            image={imagen}
+            rate={'10'}
+            direction={'Car'}
+          />
+        } />
       </Routes>
   );
 }
