ba6f6520325154cdbf9eae075be4cd36b6457fbb
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-16 18:58:57 +0200

    routes

--- movies_db/src/App.jsx
+++ movies_db/src/App.jsx
@@ -1,23 +1,16 @@
 import './App.css'
-import { MovieCard }from './components/MovieCard';
 import { Welcome } from './components/Welcome'
+import { MoviesList } from './pages/MoviesList'
+
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import imagen from './img/cank.jpg'
 // import { Link } from 'react-router-dom'
 
 function App() {
-
   return (
       <Routes>
-        <Route path="/" element={<Welcome username={"Oriana"} />} />
-        <Route path="/movies" element={
-          <MovieCard 
-            title={"Cank"} 
-            image={imagen}
-            rate={'10'}
-            direction={'Car'}
-          />
-        } />
+        <Route path="/" element={ <Welcome username={"Oriana"} /> } />
+        <Route path="/movies/list" element={ <MoviesList /> } />
+        <Route path="/movies/add" element={ <h1>ADD</h1> } />
       </Routes>
   );
 }
