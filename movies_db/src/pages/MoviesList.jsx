da2194960b9ec060417be22e32c88003cc4e11a4
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-18 03:13:51 +0200

    Movie list

--- movies_db/src/pages/MoviesList.jsx
+++ movies_db/src/pages/MoviesList.jsx
@@ -1,42 +1,46 @@
-import { MovieCard } from '../components/MovieCard';
+import { MovieCard } from '../components/MovieCard'
 import imagen from '../img/cank.jpg'
+import { useEffect, useState } from "react";
+import db from "../config/config";
+import { collection , deleteDoc, doc, getDocs} from "firebase/firestore";
 
-// const moviesData = [
-//     {
-//       id: 1,
-//       title: "Inception",
-//       image: imagen,
-//       rate: "8.8",
-//       direction: "Christopher Nolan"
-//     },
-//     {
-//       id: 2,
-//       title: "The Shawshank Redemption",
-//       image: imagen,
-//       rate: "9.3",
-//       direction: "Frank Darabont"
-//     },
-//     {
-//       id: 3,
-//       title: "The Godfather",
-//       image: imagen,
-//       rate: "9.2",
-//       direction: "Francis Ford Coppola"
-//     },
-//     // Agrega más películas si lo deseas
-//   ];
+const moviesData = [
+    {
+      id: 1,
+      title: "Inception",
+      image: imagen,
+      rate: "8.8",
+      direction: "Christopher Nolan"
+    },
+    {
+      id: 2,
+      title: "The Shawshank Redemption",
+      image: imagen,
+      rate: "9.3",
+      direction: "Frank Darabont"
+    },
+    {
+      id: 3,
+      title: "The Godfather",
+      image: imagen,
+      rate: "9.2",
+      direction: "Francis Ford Coppola"
+    }  
+];
 
 export function MoviesList() {
 
     return (
-        <div className="moviesListComp">
+        <div className="moviesListComp" >
+          {moviesData.map(movie => (
             <MovieCard
-              key={"1"}
-              title={"Cank"}
-              image={imagen}
-              rate={"10/10"}
-              direction={"Cank itseft"}
+              key={movie.id}
+              title={movie.title} 
+              image={movie.image}
+              rate={movie.rate}
+              direction={movie.direction}
             />
+          ))}
         </div>
       );
 }
