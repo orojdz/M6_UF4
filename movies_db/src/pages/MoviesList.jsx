e52611fff42ff054a871571e1e4c5de3e4a05523
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-18 23:59:38 +0200

    avance

--- movies_db/src/pages/MoviesList.jsx
+++ movies_db/src/pages/MoviesList.jsx
@@ -1,8 +1,11 @@
 import { MovieCard } from '../components/MovieCard'
 import imagen from '../img/cank.jpg'
-import { useEffect, useState } from "react";
-import db from "../config/config";
-import { collection , deleteDoc, doc, getDocs} from "firebase/firestore";
+import { Link } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
+import HomeIcon from '@mui/material/HomeIcon';
+// import { useEffect, useState } from "react";
+// import db from "../config/config";
+// import { collection , deleteDoc, doc, getDocs} from "firebase/firestore";
 
 const moviesData = [
     {
@@ -25,22 +28,49 @@ const moviesData = [
       image: imagen,
       rate: "9.2",
       direction: "Francis Ford Coppola"
-    }  
+    },
+    {
+      id: 3,
+      title: "The Godfather",
+      image: imagen,
+      rate: "9.2",
+      direction: "Francis Ford Coppola"
+    },
+    {
+      id: 3,
+      title: "The Godfather",
+      image: imagen,
+      rate: "9.2",
+      direction: "Francis Ford Coppola"
+    },
+    {
+      id: 3,
+      title: "The Godfather",
+      image: imagen,
+      rate: "9.2",
+      direction: "Francis Ford Coppola"
+    }     
 ];
 
 export function MoviesList() {
 
     return (
-        <div className="moviesListComp" >
-          {moviesData.map(movie => (
-            <MovieCard
-              key={movie.id}
-              title={movie.title} 
-              image={movie.image}
-              rate={movie.rate}
-              direction={movie.direction}
-            />
-          ))}
+        <div>
+            <Link to='/'>
+              <h2>Lista de películas</h2>
+            </Link>
+        
+          <div className="moviesListComp" >
+            {moviesData.map(movie => (
+              <MovieCard
+                key={movie.id}
+                title={movie.title} 
+                image={movie.image}
+                rate={movie.rate}
+                direction={movie.direction}
+              />
+            ))}
+          </div>
         </div>
       );
 }
