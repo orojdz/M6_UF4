77027e990531e8f792a2bff6bde03542d6083c29
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-18 03:14:19 +0200

    welcome copm wip

--- movies_db/src/components/Welcome.jsx
+++ movies_db/src/components/Welcome.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { IndexMenu } from "../pages/IndexMenu";
 
 export function Welcome(prop) {
     return(
       <>
         <h1>Bienvenido { prop.username }</h1>
         <h2>Pel·lícules per a l’estiu.</h2>
+
+        <IndexMenu/>
       </>
     );
 }
