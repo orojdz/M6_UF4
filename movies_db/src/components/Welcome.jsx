f05beb094d2aad28beae2d214beec04c5a3b42ea
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-18 23:59:46 +0200

    avance

--- movies_db/src/components/Welcome.jsx
+++ movies_db/src/components/Welcome.jsx
@@ -1,12 +1,11 @@
 import React from "react"
 import { IndexMenu } from "../pages/IndexMenu";
 
-export function Welcome(prop) {
+export function Welcome({ username }) {
     return(
       <>
-        <h1>Bienvenido { prop.username }</h1>
+        <h1>Bienvenido/a { username }</h1>
         <h2>Pel·lícules per a l’estiu.</h2>
-
         <IndexMenu/>
       </>
     );
