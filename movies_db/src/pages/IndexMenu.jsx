c43f3e7510ce7ce767f28439c8e2e77047fc7460
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-18 03:14:03 +0200

    WIP - Index

--- movies_db/src/pages/IndexMenu.jsx
+++ movies_db/src/pages/IndexMenu.jsx
@@ -1,20 +1,21 @@
 import { CardComp } from '../components/Card'
+import Card from '@mui/material/Card';
+import { CardActionArea } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export function IndexMenu() {
     return(
-        <div>
-            <CardComp
-                title={title} 
-                image={image}
-                rate={""}
-                direction={""}
-            />
-            <CardComp
-                title={title} 
-                image={image}
-                rate={""}
-                direction={""}
-            />
+        <div className='select'>
+            <CardComp size={400}>
+                <Card className="toList" sx={{ maxWidth: 400 }}>
+                    <Link to="/movies/list">Lista de peliculas</Link>
+                </Card>
+            </CardComp>
+            <CardComp size={400}>
+                <Card className="toList" sx={{ maxWidth: 400 }}>
+                    <Link to="/movies/add">Añadir una pelicula</Link>
+                </Card>
+            </CardComp> 
         </div>
     );
 }
