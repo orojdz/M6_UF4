e1c152aef36dd6364d4f5c54f2072066c57c3bab
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-19 02:14:45 +0200

    main app routes

--- movies_db/src/App.jsx
+++ movies_db/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
+import { MoviesAdd } from './components/MoviesAdd';
 import { Welcome } from './components/Welcome'
 import { MoviesList } from './pages/MoviesList'
-
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-// import { Link } from 'react-router-dom'
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={ <Welcome username={"Oriana"} /> } />
         <Route path="/movies/list" element={ <MoviesList /> } />
-        <Route path="/movies/add" element={ <h1>ADD</h1> } />
+        <Route path="/movies/add" element={ <MoviesAdd /> } />
       </Routes>
   );
 }
