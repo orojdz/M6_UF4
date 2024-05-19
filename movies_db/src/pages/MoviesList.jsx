bebca384a1dd540e8509895a37cfa4ee5fb91b40
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-19 02:15:26 +0200

    Lista de peliculas funciona, falta firebase

--- movies_db/src/pages/MoviesList.jsx
+++ movies_db/src/pages/MoviesList.jsx
@@ -1,8 +1,7 @@
 import { MovieCard } from '../components/MovieCard'
 import imagen from '../img/cank.jpg'
 import { Link } from 'react-router-dom';
-import IconButton from '@mui/material/IconButton';
-import HomeIcon from '@mui/material/HomeIcon';
+import Typography from '@mui/material/Typography';
 // import { useEffect, useState } from "react";
 // import db from "../config/config";
 // import { collection , deleteDoc, doc, getDocs} from "firebase/firestore";
@@ -16,61 +15,71 @@ const moviesData = [
       direction: "Christopher Nolan"
     },
     {
-      id: 2,
-      title: "The Shawshank Redemption",
+      id: 1,
+      title: "Inception",
       image: imagen,
-      rate: "9.3",
-      direction: "Frank Darabont"
+      rate: "8.8",
+      direction: "Christopher Nolan"
+    },{
+      id: 1,
+      title: "Inception",
+      image: imagen,
+      rate: "8.8",
+      direction: "Christopher Nolan"
+    },{
+      id: 1,
+      title: "Inception",
+      image: imagen,
+      rate: "8.8",
+      direction: "Christopher Nolan"
+    },{
+      id: 1,
+      title: "Inception",
+      image: imagen,
+      rate: "8.8",
+      direction: "Christopher Nolan"
+    },{
+      id: 1,
+      title: "Inception",
+      image: imagen,
+      rate: "8.8",
+      direction: "Christopher Nolan"
     },
-    {
-      id: 3,
-      title: "The Godfather",
-      image: imagen,
-      rate: "9.2",
-      direction: "Francis Ford Coppola"
-    },
-    {
-      id: 3,
-      title: "The Godfather",
-      image: imagen,
-      rate: "9.2",
-      direction: "Francis Ford Coppola"
-    },
-    {
-      id: 3,
-      title: "The Godfather",
-      image: imagen,
-      rate: "9.2",
-      direction: "Francis Ford Coppola"
-    },
-    {
-      id: 3,
-      title: "The Godfather",
-      image: imagen,
-      rate: "9.2",
-      direction: "Francis Ford Coppola"
-    }     
 ];
 
 export function MoviesList() {
 
-    return (
-        <div>
-            <Link to='/'>
-              <h2>Lista de películas</h2>
-            </Link>
-        
-          <div className="moviesListComp" >
-            {moviesData.map(movie => (
-              <MovieCard
-                key={movie.id}
-                title={movie.title} 
-                image={movie.image}
-                rate={movie.rate}
-                direction={movie.direction}
-              />
-            ))}
-          </div>
+  return (
+      <div>
+        <Link to='/'>
+          <Typography
+            gutterBottom 
+              variant="h2"
+                component="div"
+                className="typography-title"
+                sx={{
+                    whiteSpace: 'nowrap',
+                    maxWidth: '100%',
+                    fontFamily: 'Roboto',
+                    fontWeight: 700,
+                    fontSize: '1.25rem',
+                }}>
+
+            <h2>Llista de pel·lícules</h2>
+          </Typography>
+        </Link>
+
+        <div className="moviesListComp" >
+          {moviesData.map(movie => (
+            <MovieCard
+              key={movie.id}
+              title={movie.title} 
+              image={movie.image}
+              rate={movie.rate}
+              direction={movie.direction}
+            />
+          ))}
         </div>
-      );
+      </div>
+  );
 }
