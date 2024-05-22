44e0cc3fdcd0eb513611793bba5d9b863cf8e21b
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-22 02:35:19 +0200

    WIP - Funciona

--- movies_db/src/pages/MoviesList.jsx
+++ movies_db/src/pages/MoviesList.jsx
@@ -1,53 +1,33 @@
 import { MovieCard } from '../components/MovieCard'
-import imagen from '../img/cank.jpg'
+// import imagen from '../img/cank.jpg'
 import { Link } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
-// import { useEffect, useState } from "react";
-// import db from "../config/config";
-// import { collection , deleteDoc, doc, getDocs} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import db from "../config/config";
+import { collection, doc, getDocs, deleteDoc } from "firebase/firestore";
 
-const moviesData = [
-    {
-      id: 1,
-      title: "Inception",
-      image: imagen,
-      rate: "8.8",
-      direction: "Christopher Nolan"
-    },
-    {
-      id: 1,
-      title: "Inception",
-      image: imagen,
-      rate: "8.8",
-      direction: "Christopher Nolan"
-    },{
-      id: 1,
-      title: "Inception",
-      image: imagen,
-      rate: "8.8",
-      direction: "Christopher Nolan"
-    },{
-      id: 1,
-      title: "Inception",
-      image: imagen,
-      rate: "8.8",
-      direction: "Christopher Nolan"
-    },{
-      id: 1,
-      title: "Inception",
-      image: imagen,
-      rate: "8.8",
-      direction: "Christopher Nolan"
-    },{
-      id: 1,
-      title: "Inception",
-      image: imagen,
-      rate: "8.8",
-      direction: "Christopher Nolan"
-    },
-];
+// const moviesData = [
+//     {
+//       id: 1,
+//       title: "Inception",
+//       image: imagen,
+//       rate: "8.8",
+//       direction: "Christopher Nolan"
+//     }
+// ];
 
 export function MoviesList() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getDocs(collection(db, "movies"))
+      .then((response) => {
+        setMovies(response.docs.map((doc) => { 
+          return { ...doc.data(), id:doc.id }
+          })
+        );
+      })
+  }, []);
 
   return (
       <div>
@@ -70,7 +50,7 @@ export function MoviesList() {
         </Link>
 
         <div className="moviesListComp" >
-          {moviesData.map(movie => (
+          {movies.map(movie => (
             <MovieCard
               key={movie.id}
               title={movie.title} 
