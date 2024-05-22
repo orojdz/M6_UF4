0d5a72f9d6d5317882bf924818d637f992517475
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-22 02:35:09 +0200

    imagenes añadidas

--- movies_db/src/pages/IndexMenu.jsx
+++ movies_db/src/pages/IndexMenu.jsx
@@ -1,6 +1,8 @@
 import { CardComp } from '../components/Card'
 import Card from '@mui/material/Card';
 import { Link } from 'react-router-dom';
+import filmGif from '../img/film-tape.gif';
+import cameraGif from '../img/camera.gif';
 
 export function IndexMenu() {
     return(
@@ -8,8 +10,9 @@ export function IndexMenu() {
             <div className="toList">
                 <CardComp>
                     <Link to="/movies/list">
-                        <Card sx={{ minWidth: 250, minHeight: 250}}>
+                        <Card sx={{ minWidth: 250, minHeight: 250 }}>
                             <h4>Llista de pel·lícules</h4>
+                            <img src={filmGif} alt="llista de pel·lícules" width={150}/>
                         </Card>
                     </Link>
                 </CardComp>
@@ -19,6 +22,7 @@ export function IndexMenu() {
                     <Link to="/movies/add">
                         <Card sx={{ minWidth: 250, minHeight: 250 }}>
                             <h4>Afegeix una pel·lícula</h4>
+                            <img src={cameraGif} alt="Afegir una pel·lícula" width={150} />
                         </Card>
                     </Link>
                 </CardComp> 
