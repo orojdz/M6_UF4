bdcd701d964b174f3d6afcd79deb902797befdd6
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-23 23:59:37 +0200

    app

--- movies_db/src/App.jsx
+++ movies_db/src/App.jsx
@@ -3,14 +3,18 @@ import { MoviesAdd } from './components/MoviesAdd';
 import { Welcome } from './components/Welcome'
 import { MoviesList } from './pages/MoviesList'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Navbar } from './components/NavBar';
 
 function App() {
   return (
+    <>
+    <Navbar />
       <Routes>
         <Route path="/" element={ <Welcome username={"Oriana"} /> } />
         <Route path="/movies/list" element={ <MoviesList /> } />
         <Route path="/movies/add" element={ <MoviesAdd /> } />
       </Routes>
+    </>
   );
 }
 
