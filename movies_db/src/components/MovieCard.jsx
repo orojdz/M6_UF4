86da2621dab0aab5a273cc47c3b81d3746325741
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-23 23:59:48 +0200

    Movie card comp

--- movies_db/src/components/MovieCard.jsx
+++ movies_db/src/components/MovieCard.jsx
@@ -1,43 +1,43 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import DeleteIcon from '@mui/icons-material/Delete';
+import HeartIcon from './HeartIcon';
 import { CardComp } from './Card';
 
-export function MovieCard({ title, image, rate, direction }) {
+export function MovieCard({ id, title, year, image, rate, director, description, duration, onDelete }) {
+      
+    // Función para abreviar la descripción de la película
+    const abbrDesc = (description) => {
+       if (description.length > 20)
+            return description.substring(0, 50) + '...'
+    }
+
     return (
-        <CardComp size={200}>
-            <Card className="cards" sx={{ maxWidth: 280 }}>
-                <CardMedia sx={{ height: 290 }}
-                  image={image}
-                  title={title}
-                />
-                <CardContent>
-                    <Typography 
-                        gutterBottom 
-                        variant="h5"
-                        component="div"
-                        className="typography-title"
-                        sx={{
-                            whiteSpace: 'nowrap',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            maxWidth: '100%',
-                            fontFamily: 'Roboto',
-                            fontWeight: 700,
-                            fontSize: '1.25rem'
-                        }}>
-                        {title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ fontFamily: 'Roboto'}}>
-                        {direction}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ fontFamily: 'Roboto'}}>
-                        {rate}
-                    </Typography>
-                </CardContent>
-            </Card>
-            
+        <CardComp size={300}>
+            <div className="movie-card">
+            <div className="movie-icons">
+                <DeleteIcon sx={{ color: 'grey' }} className='icon' onClick={() => onDelete(id)} />
+            </div>
+            <img src={image} alt={title} className="movie-card-image" />
+            <div className="movie-card-overlay">
+                <div className="movie-card-header">
+                    <h3 className="movie-title">{title} ({year})</h3>
+                    <div className='icon-info'>
+                        <div className='rate'>
+                            <small>{rate}</small><StarRateIcon className='rate-star' sx={{ color: 'yellow' }} fontSize='smaller' />
+                        </div>
+                        <div className='duration'>
+                            <small>{duration}m</small>
+                        </div>
+                    </div>
+                </div>
+                <div className="movie-card-body">
+                    <small className='movie-director'><strong>{director}</strong></small>
+                    <div className='desc-heart'>
+                        <p className="movie-description">{ abbrDesc(description)}</p> <HeartIcon />
+                    </div>
+                </div>
+            </div>
+            </div>
         </CardComp>
     );
 }
